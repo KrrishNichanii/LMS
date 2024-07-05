aac12c74bff4e6729256c871f39d78d91ee03122
KrrishNichanii <krrishnichanii@gmail.com>
2024-07-05 16:59:42 +0530

    Chat Server added Frontend URL

--- server/src/chatServer.js
+++ server/src/chatServer.js
@@ -15,7 +15,7 @@ let waitingStudents = [] ;
 
 const io = new Server(server,{
     cors:{
-        origin: "http://localhost:5173" , 
+        origin: process.env.FRONTEND_URL || '*' , 
         methods:['GET',"POST"]
     }
 })
